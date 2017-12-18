35ffcb062702265842a1a8636d37ce7ca686a8e9
Stefan <stefantrysil@gmail.com>
2017-12-18 18:49:44 +0100

    bug(async) wrapped async code in a try catch block, delegating error handling to express through next function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,32 +6,52 @@ const router = express.Router();
 
 const requireLogin = require('../middlewares/requireLogin');
 
-router.get('/pin/:id', async (req, res) => {
-  res.send(await Pin.findById(req.params.id));
+router.get('/pin/:id', async (req, res, next) => {
+  try {
+    res.send(await Pin.findById(req.params.id));
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/pin/:id/edit', async (req, res) => {
-  res.json(await Pin.findOneAndUpdate(
-    { _id: req.params.id },
-    req.body,
-    { new: true, runValidators: true }
-  ).exec());
+router.post('/pin/:id/edit', async (req, res, next) => {
+  try {
+    res.json(await Pin.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true, runValidators: true }
+    ).exec());
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/pin/:id', async (req, res) => {
-  await Pin.findById(req.params.id).remove().exec();
+router.delete('/pin/:id', async (req, res, next) => {
+  try {
+    await Pin.findById(req.params.id).remove().exec();
+  } catch (err) {
+    next(err);
+  }
   res.send(`pin with id ${id} has been deleted`);
 });
 
-router.post('/pin', async (req, res) => {
+router.post('/pin', async (req, res, next) => {
   req.body.author = req.user._id;
-  const pin = await (new Pin(req.body)).save();
+  try {
+    const pin = await (new Pin(req.body)).save();
+  } catch (err) {
+    next(err);
+  }
 
   res.json(pin);
 });
 
 router.get('/pins', async (req, res) => {
-  res.send(await Pin.find({}));
+  try {
+    res.send(await Pin.find({}));
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
