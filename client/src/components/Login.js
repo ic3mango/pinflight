85039375d42c2bb5548ee42e214095729b25f2d0
Stefan <stefantrysil@gmail.com>
2017-12-25 15:18:50 +0100

    client(feat) implement form for profile and landing page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,6 @@
 import React from 'react'
 
 export default (props) =>
-  <div>Login</div>
+  <div className="container d-flex justify-content-center">
+    <a className="btn btn-danger" href="/auth/google">Login With Google</a>
+  </div>
