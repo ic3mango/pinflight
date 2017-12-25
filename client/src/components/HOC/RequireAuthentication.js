4a30f95d40038e134152d8ce4cf11fa21d39ddf6
Stefan <stefantrysil@gmail.com>
2017-12-25 22:58:52 +0100

    client(feat) protected routes now redirects to Login component

---
+++ client/src/components/HOC/RequireAuthentication.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+
+
+export default function(ComposedComponent) {
+  class Authentication extends Component {
+    render() {
+      if (!this.props.user)
+        this.props.history.push('/login');
+        return null;
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps({ auth }) {
+    return { user: auth}
+  }
+  return connect(mapStateToProps)(Authentication);
+}
